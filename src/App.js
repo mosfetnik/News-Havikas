cb28b0271ebe303b72af523cd1318f23b602aa1e
mosfetNik <adityarai424@gmail.com>
2024-06-15 20:39:14 +0530

    chanses

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import NavBar from "./components/NavBar/NavBar";
 import News from "./components/News/News";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import { router } from "./config/config";
 import Search from "./components/Search/Search";
 
 function App() {
   return (
     <>
+    <BrowserRouter basename="/News-Havikas">
       <Router>
         <NavBar />
         <Routes>
@@ -29,6 +30,7 @@ function App() {
           <Route path="/search/:query" element={<Search />} />
         </Routes>
       </Router>
+      </BrowserRouter>
     </>
   );
 }
