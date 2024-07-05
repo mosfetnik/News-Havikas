6bb59d4c53afaa919c0c467f4e643fb635e1682f
mosfetNik <adityarai424@gmail.com>
2024-07-05 13:55:56 +0530

    test again

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ export const searching = "Searching...";
 export const arrow = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
 
 export const navs = [
-  { nav: "Home", page: "/" },
+  { nav: "Home", page: "/News-Havikas" },
   { nav: "General", page: "/categories/general" },
   { nav: "Business", page: "/categories/business" },
   { nav: "Sports", page: "/categories/sports" },
