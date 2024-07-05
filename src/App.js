8048982b38fa83912b671732b6d143a93b9d79bb
mosfetNik <adityarai424@gmail.com>
2024-07-05 13:10:08 +0530

    check

--- src/App.js
+++ src/App.js
@@ -2,34 +2,39 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import NavBar from "./components/NavBar/NavBar";
 import News from "./components/News/News";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import { router } from "./config/config";
 import Search from "./components/Search/Search";
 
 function App() {
   return (
     <>
-    <BrowserRouter basename="/News-Havikas">
-      <Router>
-        <NavBar />
-        <Routes>
-          {router.map((path) => (
-            <Route
-              exact
-              key={uuidv4()}
-              path={path.path}
-              element={
-                <News
-                  key={path.key}
-                  newscategory={path.category}
-                  country={path.country}
-                />
-              }
-            />
-          ))}
-          <Route path="/search/:query" element={<Search />} />
-        </Routes>
-      </Router>
+      <BrowserRouter basename="/">
+
+          <NavBar />
+          <Routes>
+            {router.map((path) => (
+              <Route
+                exact
+                key={uuidv4()}
+                path={path.path}
+                element={
+                  <News
+                    key={path.key}
+                    newscategory={path.category}
+                    country={path.country}
+                  />
+                }
+              />
+            ))}
+            <Route path="/search/:query" element={<Search />} />
+          </Routes>
+     
       </BrowserRouter>
     </>
   );
